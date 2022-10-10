083f9c94c231a71e946b7d0d3e015dca5f5f9e49
liulihua <llh11887@163.com>
2022-10-10 10:23:06 +0800

    练习输入输出流

--- src/main/java/Year_2022/Month_09/Day_13/Demo02/Demo01Properties.java
+++ src/main/java/Year_2022/Month_09/Day_13/Demo02/Demo01Properties.java
@@ -1,5 +1,8 @@
 package Year_2022.Month_09.Day_13.Demo02;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Properties;
 import java.util.Set;
 
@@ -17,9 +20,68 @@ import java.util.Set;
  *
  */
 public class Demo01Properties {
-    public static void main(String[] args) {
-        show01();
+    public static void main(String[] args) throws IOException {
+        show03();
     }
+    /**
+     * 可以使用Properties集合中的方法load,把硬盘中保存的文件（键值对），读取到集合中使用
+     *      void load(InputStream inStream)
+     *      load(Reader reader)
+     *      参数：
+     *          InputStream inStream :字节输出流，不能读取含有中文的键值对
+     *          Reader reader :字符输入流，能读取含有中文的键值对
+     *      使用步骤：
+     *          1.使用Properties集合对象
+     *          2.使用Properties集合对象中的方法load读取保存键值对的文件
+     *          3.遍历Properties集合对象
+     *      注意：
+     *          1.存储键值对的文件中，键与值默认的连接符号可以使用=，空格（其他符号）
+     *          2.存储键值对的文件中，可以使用#进行注释，被注释的键值对不会再读取
+     *          3.存储键值对的文件中中，键与值默认都是字符串，不用再加引号
+     * */
+    private static void show03() throws IOException {
+        //1.使用Properties集合对象
+        Properties prop = new Properties();
+        //2.使用Properties集合对象中的方法load读取保存键值对的文件
+//        prop.load(new FileReader("E:\\home\\yj\\test\\file\\porp.txt"));
+//        prop.load(new FileInputStream("E:\\home\\yj\\test\\file\\porp.txt"));//字节输入流读取中文会乱码
+        //3.遍历Properties集合对象
+        Set<String> set = prop.stringPropertyNames();
+        for (String key : set) {
+            String property = prop.getProperty(key);
+            System.out.println(key+"="+property );
+        }
+
+    }
+
+    /**
+     * 可以使用Properties集合中的方法store,把集合中的临时数据，持久化写入到硬盘中存储
+     * void store(OutputStream out,String comments)
+     * store(Writer writer,String comments)
+     *  参数：
+     *      OutputStream out: 字节输出流，不能写入中文
+     *      String comments:  注释，用来解释说明保存的文件是做什么用的
+     *                  不能使用中文，会缠身乱码，默认是Unicode编码
+     *                  一般使用空字符串
+     * 使用步骤：
+     *      1.创建Properties 集合对象，添加数据
+     *      2.创建字节输出流/字符输出流对象，构造方法中绑定要输出的目的地
+     *      3.使用Properties集合中的方法store，把集合中的临时数据，持久化写入到硬盘中存储(字符流可以写中文，字节流不能写中文)
+     *      4.释放资源
+     * */
+    private static void show02() throws IOException {
+        //创建Properties 集合对象，添加数据
+        Properties porp = new Properties();
+        porp.setProperty("远辰","176");
+        porp.setProperty("知非","176");
+        porp.setProperty("xxx","175");
+        //创建字节输出流/字符输出流对象，构造方法中绑定要输出的目的地
+        FileWriter fw = new FileWriter("E:\\home\\yj\\test\\file\\porp.txt");
+//        porp.store(fw,"Save data");
+//        fw.close();
+        porp.store(Files.newOutputStream(Paths.get("E:\\home\\yj\\test\\file\\port2.txt")),"");
+    }
+
     /**
      * 使用Properties集合存储数据，遍历出Properties集合中的数据
      * Properties集合是一个双列集合，key和value默认都是字符串
