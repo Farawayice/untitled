92ce1ea0a38366107f4812821e8cc70fb2da2526
liulihua <llh11887@163.com>
2022-10-27 15:34:28 +0800

    数采项目模拟类

---
+++ src/main/java/Test/workTest/data.java
@@ -0,0 +1,27 @@
+package Test.workTest;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+/**
+ * @author liulihua
+ * @date 2022/10/20 17:21
+ */
+public class data {
+
+    private List<String> polIds;
+
+    private String view;
+
+    private String exclude;
+
+    private String name;
+
+    private String typeName;
+
+    private Integer flag;
+
+    private Timestamp startTime;
+
+    private Timestamp endTime;
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,22 @@
-package org.example;/**
+package org.example;
+
+import java.sql.Timestamp;
+import java.util.*;
+
+/**
 *
 *   @DATA ${DATE}
 *   @Author liulihua
 * */public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<String> list = new ArrayList<>();
+        list.set(0,"1");
+        list.set(1,"2");
+        list.set(3,"3");
+        list.set(2,"4");
+        list.set(4,"5");
+        for (String S : list) {
+            System.out.println(S);
+        }
     }
 }
